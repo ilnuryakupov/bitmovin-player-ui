80fbf6a2b2e53652a6ad044f73e99ef26c9d7db3
Ilnur Yakupov <ilnur.yakupov@gmail.com>
2019-11-22 23:36:29 +0300

    Add 0.75 and 1.25 playback speeds

--- src/ts/components/playbackspeedselectbox.ts
+++ src/ts/components/playbackspeedselectbox.ts
@@ -12,7 +12,7 @@ export class PlaybackSpeedSelectBox extends SelectBox {
 
   constructor(config: ListSelectorConfig = {}) {
     super(config);
-    this.defaultPlaybackSpeeds = [0.25, 0.5, 1, 1.5, 2];
+    this.defaultPlaybackSpeeds = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2];
 
     this.config = this.mergeConfig(config, {
       cssClasses: ['ui-playbackspeedselectbox'],
